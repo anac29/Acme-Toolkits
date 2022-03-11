9826c85ba35fd726fc8f0fd58e7458fe1c93a76c
Jaime Moscoso Bernal <jaimesfc@live.com>
2022-03-11 19:11:06 +0100

    Task 030: Done

---
+++ src/main/java/acme/roles/Inventor.java
@@ -0,0 +1,25 @@
+package acme.roles;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import acme.framework.roles.UserRole;
+
+public class Inventor extends UserRole {
+	// Serialisation identifier -----------------------------------------------
+
+		protected static final long	serialVersionUID	= 1L;
+
+		// Attributes -------------------------------------------------------------
+
+		@NotBlank
+		@Size(min=1,max=101)
+		protected String			company;
+
+		@NotBlank
+		@Size(min=1,max=256)
+		protected String			statement;
+		
+		protected String			link;
+}
+
