c4d3f3cb4b50b360ee2168d21ac60311d48bdd4a
JaimeBorregoConde <80269612+JaimeBorregoConde@users.noreply.github.com>
2022-03-12 18:54:40 +0100

    Task 036: Done

--- src/main/java/acme/currencies/Currency.java
+++ src/main/java/acme/currencies/Currency.java
@@ -1,6 +1,7 @@
 package acme.currencies;
 
 import javax.persistence.Entity;
+import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotNull;
 
 import acme.framework.entities.AbstractEntity;
@@ -14,6 +15,7 @@ public class Currency extends AbstractEntity {
 
 	protected static final long serialVersionUID = 1L;
 	@NotNull
+	@Digits(integer=Integer.MAX_VALUE, fraction=2)
 	private Double amount;
 	@NotNull
 	private CurrencyType currencyType;
