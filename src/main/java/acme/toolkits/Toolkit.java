ef40c4a26e4df1ffab7600c8ec45621f51f480a8
anac29 <anaconmar@alum.us.es>
2022-03-12 12:29:35 +0100

    Task 035: done
    Created Toolkit entity, need to add relations.

---
+++ src/main/java/acme/toolkits/Toolkit.java
@@ -0,0 +1,46 @@
+package acme.toolkits;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.entities.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Toolkit extends AbstractEntity {
+	
+	protected static final long serialVersionUID = 1L;
+	
+	@NotNull
+	@NotBlank(message="Code ismandatory")
+	@Pattern(regexp="^[A-Z]{3}-[0-9]{3}(-[A-Z])?$")
+	private String code;
+	
+	@Range(min=0, max=101)
+	@NotBlank
+	@NotNull
+	private String title;
+	
+	@Range(min=0, max=256)
+	@NotBlank
+	@NotNull
+	private String description;
+	
+	@Range(min=0, max=256)
+	@NotBlank
+	@NotNull
+	private String assemblyNotes;
+	
+	@URL
+	private String link;
+	
+
+}
