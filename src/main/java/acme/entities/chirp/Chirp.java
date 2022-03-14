353d88efc71225172285ec485e002909d419991b
JaimeBorregoConde <80269612+JaimeBorregoConde@users.noreply.github.com>
2022-03-14 21:11:48 +0100

    Task 045: In progress.
    Add spam list with strong and weak spam terms, and its translation into
    spanish lenguaje.

--- src/main/java/acme/entities/chirp/Chirp.java
+++ src/main/java/acme/entities/chirp/Chirp.java
@@ -8,8 +8,9 @@ import javax.persistence.TemporalType;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Past;
+
 import org.hibernate.validator.constraints.Length;
+
 import acme.framework.entities.AbstractEntity;
 import lombok.Getter;
 import lombok.Setter;
@@ -28,7 +29,6 @@ public class Chirp extends AbstractEntity {
 
 
 	@Temporal(TemporalType.TIMESTAMP)
-	@Past
 	@NotNull
 	protected Date creationMoment;
 
