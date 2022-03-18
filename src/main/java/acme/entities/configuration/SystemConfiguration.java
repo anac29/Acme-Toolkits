97432478e575597d1a3bdabc030a71abdb9d09ec
gonmarfer2 <martinezfernandezgonzalo@gmail.com>
2022-03-18 18:48:53 +0100

    Task-049: created SystemConfiguration entity
    It contains the system default properties like spam words, threshold and
    currencies.

---
+++ src/main/java/acme/entities/configuration/SystemConfiguration.java
@@ -0,0 +1,40 @@
+package acme.entities.configuration;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Range;
+
+import acme.framework.entities.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class SystemConfiguration extends AbstractEntity {
+
+	// ------------------------------------------------------------------
+	private static final long serialVersionUID = 1L;
+	// ------------------------------------------------------------------
+	
+	@NotNull
+	protected String strongSpamTerms;
+	
+	@NotNull
+	protected String weakSpanTerms;
+	
+	@NotBlank
+	protected String acceptedCurrencies;
+	
+	@NotBlank
+	protected String defaultCurrency;
+	
+	@Range(min=0,max=100)
+	protected double weakThreshold;
+	
+	@Range(min=0,max=100)
+	protected double strongThreshold;
+	
+}
