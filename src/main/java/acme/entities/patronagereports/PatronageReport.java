f17f1e0ca66308379811daf0e27ae15143b9e1d8
gonmarfer2 <martinezfernandezgonzalo@gmail.com>
2022-03-20 14:03:33 +0100

    Task-052: added more examples to sample data
    Added variants for the sample data csv files.

--- src/main/java/acme/entities/patronagereports/PatronageReport.java
+++ src/main/java/acme/entities/patronagereports/PatronageReport.java
@@ -1,17 +1,19 @@
 package acme.entities.patronagereports;
 
 import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
+
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
+
 import acme.entities.patronage.Patronage;
 import acme.framework.entities.AbstractEntity;
 import lombok.Getter;
@@ -29,7 +31,7 @@ public class PatronageReport extends AbstractEntity {
 	// Attributes -------------------------------------------------------------
 
 	@NotBlank
-	protected String automaticSecuenceNumber;
+	protected String automaticSequenceNumber;
 	
 	@Temporal(TemporalType.TIMESTAMP)
 	@Past
