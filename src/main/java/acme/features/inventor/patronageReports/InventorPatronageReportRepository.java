3b88b5b1de3ab152cacb27a329655337dd12f46f
JaimeBorregoConde <80269612+JaimeBorregoConde@users.noreply.github.com>
2022-04-13 11:12:26 +0200

    Task 074: Done.
    List of Patronage Report of a individual inventor has been created.

---
+++ src/main/java/acme/features/inventor/patronageReports/InventorPatronageReportRepository.java
@@ -0,0 +1,16 @@
+package acme.features.inventor.patronageReports;
+
+import java.util.Collection;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import acme.entities.patronagereports.PatronageReport;
+import acme.framework.repositories.AbstractRepository;
+
+@Repository
+public interface InventorPatronageReportRepository extends AbstractRepository {
+
+	@Query("select p from PatronageReport p where p.patronage.inventor.id = :id")
+	Collection<PatronageReport> findManyPatronageReportsByInventorId(int id);
+}
