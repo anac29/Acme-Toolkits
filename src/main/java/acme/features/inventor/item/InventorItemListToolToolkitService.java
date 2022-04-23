e22344262cb11207ccbc3dda79d126a623c90069
anac29 <anaconmar@alum.us.es>
2022-04-23 18:18:32 +0200

    Task 071/R: hotfixes after after follow-up
    Changed links in order to control the authorise when accessing to some
    non public toolkit.

---
+++ src/main/java/acme/features/inventor/item/InventorItemListToolToolkitService.java
@@ -0,0 +1,56 @@
+package acme.features.inventor.item;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.entities.item.Item;
+import acme.features.any.item.AnyItemRepository;
+import acme.framework.components.models.Model;
+import acme.framework.controllers.Request;
+import acme.framework.services.AbstractListService;
+import acme.roles.Inventor;
+
+
+@Service
+public class InventorItemListToolToolkitService implements AbstractListService<Inventor, Item>  {
+	
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	protected AnyItemRepository repository;
+
+	// AbstractListService<Authenticated, Item> interface --------------
+
+
+	@Override
+	public boolean authorise(final Request<Item> request) {
+		assert request != null;
+
+		return true;
+	}
+
+	@Override
+	public Collection<Item> findMany(final Request<Item> request) {
+		assert request != null;
+
+		Collection<Item> result;
+		final int toolkitId= request.getModel().getInteger("id");
+		result = this.repository.findToolsByToolkit( toolkitId);
+
+		return result;
+	}
+
+	@Override
+	public void unbind(final Request<Item> request, final Item entity, final Model model) {
+		assert request != null;
+		assert entity != null;
+		assert model != null;
+
+		request.unbind(entity, model, "name", "code", "technology","retailPrice");
+	}
+	
+
+}
