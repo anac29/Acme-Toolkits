ad055bfe454bf461d9f2846043dbc394e7a4a94a
Enrique Muñoz <73706577+enrmp@users.noreply.github.com>
2022-04-23 21:08:07 +0200

    Task 082: Done
    Administrator Dashboard Test

--- src/main/webapp/WEB-INF/views/administrator/administrator-dashboard/form.jsp
+++ src/main/webapp/WEB-INF/views/administrator/administrator-dashboard/form.jsp
@@ -3,6 +3,9 @@
 <%@taglib prefix="jstl" uri="http://java.sun.com/jsp/jstl/core"%>
 <%@taglib prefix="acme" uri="urn:jsptagdir:/WEB-INF/tags"%>
 
+
+<acme:form readonly="true">
+
 <h1>
 	<acme:message code="administrator.dashboard.form.title" />
 </h1>
@@ -409,3 +412,4 @@
 			</tr>
 		</jstl:forEach>
 	</table>
+</acme:form>
--- src/test/java/acme/testing/administrator/administratorDashboard/AdministratorAdministratorDashboardShowTest.java
+++ src/test/java/acme/testing/administrator/administratorDashboard/AdministratorAdministratorDashboardShowTest.java
@@ -1,13 +1,17 @@
 package acme.testing.administrator.administratorDashboard;
 
 import org.junit.jupiter.api.Order;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import acme.testing.TestHarness;
 
 public class AdministratorAdministratorDashboardShowTest extends TestHarness {
 
+	@ParameterizedTest
+	@CsvFileSource(resources = "/administrator/administrator-dashboard/form.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(10) 
-	public void positiveTest() {
+	public void positiveTest(final int recordIndex) {
 		
 		super.signIn("administrator", "administrator");
 		
