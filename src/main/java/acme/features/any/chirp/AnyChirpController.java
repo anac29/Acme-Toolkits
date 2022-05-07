e6c9528bd1900a63a324ce7a5dc3af005046037f
JaimeBorregoConde <80269612+JaimeBorregoConde@users.noreply.github.com>
2022-05-07 19:17:45 +0200

    Task 91: In progress.
    Creation of a chirp is done. System confirmation is has not been created
    yet.

--- src/main/java/acme/features/any/chirp/AnyChirpController.java
+++ src/main/java/acme/features/any/chirp/AnyChirpController.java
@@ -28,6 +28,9 @@ public class AnyChirpController extends AbstractController<Any, Chirp> {
 
 	@Autowired
 	protected AnyChirpListService		listService;
+	
+	@Autowired
+	protected AnyChirpCreateService		createService;
 
 	// Constructors -----------------------------------------------------------
 
@@ -35,6 +38,7 @@ public class AnyChirpController extends AbstractController<Any, Chirp> {
 	@PostConstruct
 	protected void initialise() {
 		super.addCommand("list", this.listService);
+		super.addCommand("create", this.createService);
 	}
 
 }
