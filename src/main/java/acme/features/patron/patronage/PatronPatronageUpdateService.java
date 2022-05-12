8b1bf3144512754126268d85a95a374b7269bdf9
JaimeBorregoConde <80269612+JaimeBorregoConde@users.noreply.github.com>
2022-05-12 19:26:12 +0200

    Task 097: Minor change.
    PatronPatronage classes updated due to a misunderstood with group
    delivery and spam detector

--- src/main/java/acme/features/patron/patronage/PatronPatronageUpdateService.java
+++ src/main/java/acme/features/patron/patronage/PatronPatronageUpdateService.java
@@ -4,10 +4,8 @@ import java.util.Date;
 
 import org.apache.commons.lang3.time.DateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.spam_detector.SpamDetector;
 import org.springframework.stereotype.Service;
 
-import acme.entities.configuration.SystemConfiguration;
 import acme.entities.patronage.Patronage;
 import acme.framework.components.models.Model;
 import acme.framework.controllers.Errors;
@@ -99,12 +97,6 @@ public class PatronPatronageUpdateService implements AbstractUpdateService<Patro
 			errors.state(request, entity.getBudget().getAmount() > 0, "budget", "patron.patronage.form.error.negative-budget");
 		}
 		
-		if(!errors.hasErrors("legalStuff")) {
-			final SystemConfiguration sc = this.repository.findSystemConfiguration();
-			final SpamDetector sd = new SpamDetector(sc.getStrongSpamTerms(), sc.getWeakSpamTerms(), sc.getStrongThreshold(), sc.getWeakThreshold());
-			final boolean isLegalStuffSpam = sd.isSpam(entity.getLegalStuff());
-			errors.state(request, !isLegalStuffSpam, "legalStuff", "patron.patronage.form.error.spam");
-		}
 
 	}
 
