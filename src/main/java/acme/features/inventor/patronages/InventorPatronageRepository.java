efb9a98e6f3e753153e565f559618130f593a8be
JaimeBorregoConde <80269612+JaimeBorregoConde@users.noreply.github.com>
2022-05-14 14:04:46 +0200

    Task 097:Done
    Quick fix in inventor/patronage. Now we only show patronage that has
    been published.

--- src/main/java/acme/features/inventor/patronages/InventorPatronageRepository.java
+++ src/main/java/acme/features/inventor/patronages/InventorPatronageRepository.java
@@ -19,10 +19,10 @@ public interface InventorPatronageRepository extends AbstractRepository {
 	@Query("select p from Patronage p where p.inventor.id = :inventorId")
 	Collection<Patronage> findPatronagesByInventorId(int inventorId);
 	
-	@Query("select p from Patronage p where p.inventor.id = :inventorId and p.status=1")
+	@Query("select p from Patronage p where p.inventor.id = :inventorId and p.status=1 and p.published=1")
 	Collection<Patronage> findPatronagesAcceptedByInventorId(int inventorId);
 	
-	@Query("select p from Patronage p where p.inventor.id = :inventorId and p.status=0")
+	@Query("select p from Patronage p where p.inventor.id = :inventorId and p.status=0 and p.published=1")
 	Collection<Patronage> findPatronagesPendingByInventorId(int inventorId);
 	
 }
