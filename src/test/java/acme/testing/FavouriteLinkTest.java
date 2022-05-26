fa84159510e6fe90b108580398a9b0895751ab53
gonmarfer2 <martinezfernandezgonzalo@gmail.com>
2022-05-26 20:25:18 +0200

    Task-111: fixed tests after cleaning the project

--- src/test/java/acme/testing/FavouriteLinkTest.java
+++ src/test/java/acme/testing/FavouriteLinkTest.java
@@ -16,19 +16,58 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
 public class FavouriteLinkTest extends TestHarness {
-	
+
 	// Lifecycle management ---------------------------------------------------
 
 	// Test cases -------------------------------------------------------------
 
 	@Test
 	@Order(10)
-	public void favouriteLink() {
+	public void favouriteLinkGonzalo() {
 		super.navigateHome();
-		super.clickOnMenu("Anonymous", "Favourite link");		
-		super.checkCurrentUrl("http://www.example.com");
+		super.clickOnMenu("Anonymous", "28985178A: Martínez Fernández, Gonzalo");
+		super.checkCurrentUrl(
+				"https://www.youtube.com/watch?v=idn2UdOu5bE&list=PLkYgGmsQ2duxgqf7SI43cStQVXHCbRF9a&index=7");
 	}
-	
-	// Ancillary methods ------------------------------------------------------ 
-	
+
+	@Test
+	@Order(20)
+	public void favouriteLinkJaimeM() {
+		super.navigateHome();
+		super.clickOnMenu("Anonymous", "30270882F: Moscoso Bernal, Jaime");
+		super.checkCurrentUrl("https://www.twitch.tv/alexelcapo");
+	}
+
+	@Test
+	@Order(30)
+	public void favouriteLinkAntonio() {
+		super.navigateHome();
+		super.clickOnMenu("Anonymous", "79147298C: Campos Gil, Antonio");
+		super.checkCurrentUrl("https://consent.youtube.com/m?continue=https%3A%2F%2Fwww.youtube.com%2Fshorts%2FXec2NPpXsUI%3Fcbrd%3D1&gl=ES&m=0&pc=yt&uxe=eomty&hl=en-GB&src=1");
+	}
+
+	@Test
+	@Order(40)
+	public void favouriteLinkAna() {
+		super.navigateHome();
+		super.clickOnMenu("Anonymous", "47258017D: Conde Marrón, Ana");
+		super.checkCurrentUrl("https://www.zalando.es/mujer-home");
+	}
+
+	@Test
+	@Order(50)
+	public void favouriteLinkEnrique() {
+		super.navigateHome();
+		super.clickOnMenu("Anonymous", "49826048K: Muñoz Pérez, Enrique");
+		super.checkCurrentUrl("https://www.realbetisbalompie.es");
+	}
+
+	@Test
+	@Order(60)
+	public void favouriteLinkJaimeB() {
+		super.navigateHome();
+		super.clickOnMenu("Anonymous", "77926303A: Borrego Conde, Jaime");
+		super.checkCurrentUrl("https://www.youtube.com/watch?v=vTOurabBRxY&ab_channel=LinkSpets");
+	}
+
 }
--- src/test/java/acme/testing/any/userAccount/AnyUserAccountTest.java
+++ src/test/java/acme/testing/any/userAccount/AnyUserAccountTest.java
@@ -6,31 +6,30 @@ import org.junit.jupiter.params.provider.CsvFileSource;
 
 import acme.testing.TestHarness;
 
-public class AnyUserAccountTest extends TestHarness{
-	
+public class AnyUserAccountTest extends TestHarness {
+
 	@ParameterizedTest
 	@CsvFileSource(resources = "/any/userAccount/list.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(10)
-	public void positiveTest(final int recordIndex, final String enabled, final String name, 
-		final String surname,final String email ,final String password, final String username, 
-		final String roles, final String rolesShow) {
-		
-		super.clickOnMenu("Anonymous","List User Account");
+	public void positiveTest(final int recordIndex, final String enabled, final String name, final String surname,
+			final String email, final String password, final String username, final String roles,
+			final String rolesShow) {
+
+		super.clickOnMenu("Anonymous", "List User Account");
 		super.checkListingExists();
 		super.sortListing(0, "asc");
-		
-		super.checkColumnHasValue(recordIndex, 0, username);
-		super.checkColumnHasValue(recordIndex, 1, roles);
+		super.sortListing(1, "asc");
 
+		super.checkColumnHasValue(recordIndex, 0, name);
+		super.checkColumnHasValue(recordIndex, 1, surname);
+		super.checkColumnHasValue(recordIndex, 2, roles);
 
-		
 		super.clickOnListingRecord(recordIndex);
 		super.checkFormExists();
 		super.checkInputBoxHasValue("identity.name", name);
 		super.checkInputBoxHasValue("identity.surname", surname);
 		super.checkInputBoxHasValue("identity.email", email);
 		super.checkInputBoxHasValue("roles", rolesShow);
-		
 
 	}
 
