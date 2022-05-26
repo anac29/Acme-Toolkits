e9e9442abd9cc9461e9c250c08cd66235f113b53
Jaime Moscoso Bernal <jaimesfc@live.com>
2022-05-26 20:00:57 +0200

    Task 111: Fixed the view of the default currency on items.

--- src/main/java/acme/features/any/item/AnyItemShowService.java
+++ src/main/java/acme/features/any/item/AnyItemShowService.java
@@ -1,11 +1,14 @@
+
 package acme.features.any.item;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import acme.entities.item.Item;
+import acme.features.authenticated.moneyExchange.AuthenticatedMoneyExchangePerformService;
 import acme.framework.components.models.Model;
 import acme.framework.controllers.Request;
+import acme.framework.datatypes.Money;
 import acme.framework.roles.Any;
 import acme.framework.services.AbstractShowService;
 
@@ -16,6 +19,10 @@ public class AnyItemShowService implements AbstractShowService<Any, Item> {
 
 	@Autowired
 	protected AnyItemRepository repository;
+	
+	@Autowired
+	protected AuthenticatedMoneyExchangePerformService	moneyService;
+
 
 	// AbstractShowService<Administrator, Announcement> interface --------------
 
@@ -25,11 +32,10 @@ public class AnyItemShowService implements AbstractShowService<Any, Item> {
 		assert request != null;
 		Item item;
 		int id;
-		
+
 		id = request.getModel().getInteger("id");
 		item = this.repository.findOneItemById(id);
-		
-		
+
 		return item.isPublished();
 	}
 
@@ -45,21 +51,33 @@ public class AnyItemShowService implements AbstractShowService<Any, Item> {
 
 		return result;
 	}
-	
+
 	@Override
 	public void unbind(final Request<Item> request, final Item entity, final Model model) {
 		assert request != null;
 		assert entity != null;
 		assert model != null;
 
-		
+		final String defaultCurrency = this.repository.defaultCurrency();
+		final Double defaultPrice = this.moneyService.computeMoneyExchange(entity.getRetailPrice(), defaultCurrency).getTarget().getAmount();
+		final Money retailPriceDefault = new Money();
+		retailPriceDefault.setAmount(defaultPrice);
+		retailPriceDefault.setCurrency(defaultCurrency);
+
+		model.setAttribute("retailPriceDefault", retailPriceDefault);
+		model.setAttribute("acceptedCurrency", this.acceptedCurrencyChecker(entity));
+
 		model.setAttribute("itemId", entity.getId());
 		model.setAttribute("inventorName", entity.getInventor().getUserAccount().getIdentity().getName());
 		model.setAttribute("inventorSurname", entity.getInventor().getUserAccount().getIdentity().getSurname());
 		model.setAttribute("inventorEmail", entity.getInventor().getUserAccount().getIdentity().getEmail());
-		request.unbind(entity, model, "name", "code", "technology","description","itemType","retailPrice", "link", "inventor.userAccount.username");		
+		request.unbind(entity, model, "name", "code", "technology", "description", "itemType", "retailPrice", "link", "inventor.userAccount.username");
+	}
+	
+	public Boolean acceptedCurrencyChecker(final Item entity) {
+
+		return this.repository.findSystemConfiguration().getAcceptedCurrencies().matches("(.*)" + entity.getRetailPrice().getCurrency() + "(.*)");
+
 	}
 
-
-	
 }
